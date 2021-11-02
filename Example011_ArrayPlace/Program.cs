e9ec4dd3e96a68e8eecdc3eddae7e35ae2c4d40f
Irina <irina.filippova.mail@yandex.ru>
2021-11-02 14:16:00 +0300

    Lecture 002 02.11.2021

---
+++ Example011_ArrayPlace/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {12, 56, 22, 13, 10, 58, 22, 18, 27};
+int n = array.Length;
+int find = 22;
+int index = 0;
+
+while (index < n)
+{
+  if (array[index] == find)
+   {
+    Console.WriteLine(index);
+    break;
+   }
+    index++;
+}
+
+
