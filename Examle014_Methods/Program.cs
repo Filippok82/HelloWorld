06a0207f99d1a344fbd43834239a4595fdb64863
Irina Filippova <irina.filippova.mail@yandex.ru>
2021-11-06 13:24:30 +0300

    Lecture 03

---
+++ Examle014_Methods/Program.cs
@@ -0,0 +1,56 @@
+﻿// Метод 1
+void Method1()
+{
+    Console.WriteLine("Автор ");
+}
+/* Method1(); */
+
+
+// Метод 2
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+/*   Method2("Какой-то текст");  */
+
+// Метод 2+1
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while(i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+/* Method21("Какой-то текст", 5);   */
+/* Method21(count: 5, msg: "Текст вид метода 2");  
+  */
+ // Метод 3
+ 
+int Method3()
+{
+   return DateTime.Now.Year;
+} 
+int year = Method3();
+/* Console.WriteLine(year); */
+
+
+// Метод 4
+ 
+string Method4(int count, string text)
+{
+   int i = 0;
+   string result = String.Empty;
+
+   while(i<count)
+   {
+       result = result + text;
+       i++;
+   }  
+   return result;
+} 
+string res = Method4(10, "z");
+Console.WriteLine(res);
+
---
+++ Example0015_TaskText/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача с текстом 
+/* Дан текст, в тексте нужно все пробелы заменить черточками, 
+маленькие буквы "м", заменить большими.Большие буквы "П", маленькими.
+
+ */
+ string text = "Программист,что это значит?"
+             + "Это сильный человек!Над программой не заплачет"
+             + "а работать будет век. Пусть же разные программы"
+             + "пусть своим умом вы сами добиваетесь всего!";
+
+/* string s = qwerty
+              012345
+    s[3] = r     */   
+ string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    
+    int length = text.Length;
+    for(int i=0; i<length; i++)
+    {
+        if(text [i]==oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText); 
+newText = Replace(newText, 'м', 'М');
+Console.WriteLine(newText);
+newText = Replace(newText, 'П', 'п');
+Console.WriteLine(newText); 
+
