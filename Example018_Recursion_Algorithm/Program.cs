0538752c8f7cdb860bcafe9d2a7e4e1eb3bad925
Irina Filippova <irina.filippova.mail@yandex.ru>
2021-11-20 16:40:08 +0300

    Lecture03

---
+++ Example018_Recursion_Algorithm/Program.cs
@@ -0,0 +1,33 @@
+﻿//    строка
+
+/* string[,] table = new string[2, 5];
+// String.Empty
+//table[0,0] ...table[0,4]
+//table[1,0] ...table[1,4]
+
+table[1, 2] = " слово";
+for (int rows = 0; rows < 2; rows++) //rows строки
+{
+    for (int columns = 0; columns < 5; columns++) // columns столбцы
+    {
+        Console.WriteLine($"-{table[rows, columns]}-");
+    }
+}
+
+ */
+
+// Числа
+int[,] matrix = new int[3, 4];
+
+for (int i = 0; i < matrix.GetLength(0); i++) // строки
+{
+    for (int j = 0; j < matrix.GetLength(1); j++) // столбцы
+    {
+        Console.Write($"{matrix[i, j]} ");
+    }
+    Console.WriteLine();
+}
+
+
+
+
---
+++ Example019_Recursion_Metod/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+void PrintArray(int[,] matr)
+{
+
+    for (int i = 0; i < matr.GetLength(0); i++) // строки
+    {
+        for (int j = 0; j < matr.GetLength(1); j++) // столбцы
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void FillArray(int[,] matr)
+{
+
+    for (int i = 0; i < matr.GetLength(0); i++) // строки
+    {
+        for (int j = 0; j < matr.GetLength(1); j++) // столбцы
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+
+    }
+}
+
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
